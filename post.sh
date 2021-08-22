310d48e120695cd5a6ae61a59e83e8a5f5148e18
Audrey <audrey@jumpydoll.ze>
2021-08-22 19:58:13 +0200

    Updated script to be more flexible and include an option to pick the credentials file from which to tweet eventual posts.

--- post.sh
+++ post.sh
@@ -5,16 +5,30 @@ while [[ $# -gt 0 ]]; do
 
     case $key in
     -t|--tweet)
+        # Enables tweeting of the text files
+        # as opposed to simply displaying them
         TWEETMODE=ON
-        shift # shift past argument
+        shift # past argument
         ;;
     -f|--filename)
+        # Controls which file contains the list
+        # of filenames to select
         FNAME="$2"
-        shift # shift past argument
-        shift # shift past value
+        shift # past argument
+        shift # past value
+        ;;
+    -c|--credentials)
+        # Optionally specify twitter credentials file
+        CREDENTIALS="$2"
+        shift # past argument
+        shift # past value
         ;;
     esac
 done
+
+if [ -z ${CREDENTIALS} ]; then
+    CREDENTIALS=acc.toml
+fi
 
 if [ -z ${FNAME} ]; then
     FNAME=quotes
@@ -26,7 +40,7 @@ QUOTE=$(head -1 "${FNAME}")
 tail -n +2 "${FNAME}" > "${FNAME}.tmp" && mv "${FNAME}.tmp" "${FNAME}"
 
 if [ ${TWEETMODE} ]; then
-    tweet -c "./acc.toml" send "$(cat ${QUOTE})"
+    tweet -c "${CREDENTIALS}" send "$(cat ${QUOTE})"
 else
     cat ${QUOTE}
 fi
