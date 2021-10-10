830fa47b540607d58cb61539d45e089aa37fb904
Audrey <audrey@jumpydoll.ze>
2021-10-10 18:56:53 +0200

    Updated normalize tool to strip more than one space of text.

--- util/normalize_text.sh
+++ util/normalize_text.sh
@@ -2,7 +2,7 @@
 
 # While the length is non-zero
 while [ -n "$1" ]; do
-    sed 's/…/.../g' "$1" | sed "s/’/'/g" | sed -E 's/“|”/"/g' >.buff
+    sed 's/…/.../g' "$1" | sed "s/’/'/g" | sed -E 's/“|”/"/g' | sed -E 's/\ \ +//g' >.buff
     mv .buff "$1"
     shift
 done
