292d0a946441612642b386542d7569b33ea282c7
ironballista <87903825+ironballista@users.noreply.github.com>
2025-03-09 18:44:57 +0100

    Introduced code to interact with Redis as a permanent store for the quote queue.

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,13 @@ use bsky_sdk::BskyAgent;
 use glob::glob;
 use grep::{matcher::Matcher, regex, searcher::sinks};
 use rand::random_range;
+use rand::seq::SliceRandom;
 
 use std::{sync::Arc, time::Duration};
 use tokio::sync::Mutex;
 use tokio_cron_scheduler::{Job, JobScheduler, JobSchedulerError};
+
+use redis::{aio::{self, MultiplexedConnection}, AsyncCommands, Client};
 
 fn prepare_post<I: Into<String>>(text: I) -> post::RecordData {
     post::RecordData {
@@ -30,7 +33,7 @@ struct QuoteFilter {
     dates: Vec<String>,
 }
 
-fn read_files(filter: QuoteFilter) -> Vec<String> {
+fn read_files(filter: &QuoteFilter) -> Vec<String> {
     let matcher = regex::RegexMatcher::new(&filter.content).unwrap();
     let mut searcher = grep::searcher::Searcher::new();
     let mut results = Vec::new();
@@ -59,13 +62,33 @@ fn read_files(filter: QuoteFilter) -> Vec<String> {
     results
 }
 
+async fn reshuffle_quotes(filter: &QuoteFilter, mut con: impl redis::aio::ConnectionLike + AsyncCommands, output_queue: &str) -> Result<(), ()> {
+    let len: u64 = con.llen(output_queue).await.unwrap();
+    // NOTE: The following assumes the queue hasn't been repopulated by any other client
+    //       in-between the call to llen and the execution of the pipeline.
+    //       Hopefully won't be a problem :)
+    if len == 0 {
+        let mut file_contents = read_files(filter);
+
+        {
+            let mut rand = rand::rng();
+            file_contents.shuffle(&mut rand);
+        }
+
+        let mut pipeline = redis::pipe();
+        for file_contents in file_contents.into_iter() {
+            pipeline.lpush(output_queue,file_contents.as_str());
+        }
+        let _: () = pipeline.query_async(&mut con).await.unwrap();
+    }
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let file_contents = Arc::new(read_files(QuoteFilter {
-        content: r"\b(?i:mother|mommy|mama|mom)\b".to_string(),
-        path: "quotes/**/*.txt".to_string(),
-        dates: vec![],
-    }));
+    let redis = redis::Client::open(std::env::var("REDIS_URL").unwrap_or("redis://localhost".to_string()))?;
+    let con = redis.get_multiplexed_async_connection().await?;
 
     let agent = BskyAgent::builder().build().await?;
     let _session = agent
@@ -77,16 +100,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let sched = JobScheduler::new().await?;
     let agent = Arc::new(Mutex::new(agent));
+    let filter = Arc::new(QuoteFilter {
+        content: r"\b(?i:mother|mommy|mama|mom)\b".to_string(),
+        path: "quotes/**/*.txt".to_string(),
+        dates: vec![],
+    });
 
     // Add async job
     sched
         .add(Job::new_async("0/10,5/10 * * * * *", move |_uuid, _| {
-            let file_contents = file_contents.clone();
+            let filter = filter.clone();
+            let mut con = con.clone();
             let agent = agent.clone();
 
             Box::pin(async move {
-                let text  = file_contents[random_range(..file_contents.len())].as_str();
-                let post = prepare_post(text);
+                let _ = reshuffle_quotes(&filter, con.clone(), "test:queue").await.unwrap();
+                let text: String = con.lpop("test:queue", None).await.unwrap();
+                let post = prepare_post(text.as_str());
                 let agent = agent.lock().await;
                 if let Err(_) = agent.create_record(post).await {
                     println!("{}\n", text)
